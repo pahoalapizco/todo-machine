ac69ee03d22d63ce97465ecd49b0822ab2d4fe52
Paola Alapizco <paolac.alapizco@gmail.com>
2022-06-17 18:35:54 -0600

    Title and counter in text added, button's position modified

--- src/components/TodoCounter.js
+++ src/components/TodoCounter.js
@@ -17,8 +17,15 @@ export const TodoCounter = () => {
   }
 
   return (
-    <div className="TodoCounter--progressbar">
-      <span className="progressbar" style={progress}> {percent} </span>
-    </div>
+    <section className="TodoCounter-container">
+      <div className="TodoCounter--title">
+        <h1 className="title"> Hey <br /> There! </h1>
+        <p> Welcome to your favorite TODO app </p>
+      </div>
+      <div className="TodoCounter--progressbar">
+        <span className="progressbar" style={progress}> {percent} </span>
+      </div>
+      <p className="TodoCounter-text"> You have {completed} compleated of {total} tasks. </p>
+    </section>
   );
 }
